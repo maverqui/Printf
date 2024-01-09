20337993d0a0803e12c6fcc279bfbf9841b85320
maverqui <maeverquin13@gmail.com>
2024-01-09 18:32:45 +0100

    commit 42

--- printf.c
+++ printf.c
@@ -1,32 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printf.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maverqui <maverqui@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/09 17:33:18 by maverqui          #+#    #+#             */
+/*   Updated: 2024/01/09 17:33:18 by maverqui         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "printf.h"
 
 static int	ft_conversion(va_list arg, char nxt)
 {
 	char	c;
+	char	*str;
+	unsigned int	nb;
+	char *nb_str;
 
 	if (nxt == '%')
 		return (write(1, '%', 1));
 	else if (nxt == 'c')
 	{
-		c = va_arg(arg, int);
-		return write(1, &c, 1);
+		c = va_arg(arg, char);
+		return (write(1, &c, 1));
 	}
 	else if (nxt == 's')
-		
-	else if (nxt == 'p')
-
+	{
+		str = va_arg(arg, char*);
+		return (write(1, str, ft_strlen(str)));
+	}
+	// else if (nxt == 'p')
+	// {
+	// 	nb = va_arg(arg, unsigned int);
+	// 	return (write(1, nb, 1));
+	// }
 	else if (nxt == 'd')
-	
+	{
+		nb = va_arg(arg, int);
+		*nb_str = ft_itoa(nb);
+		return (write(1, nb_str, ft_strlen(nb_str)));
+	}
 	else if (nxt == 'i')
-
+	{
+		nb = va_arg(arg, int);
+		*nb_str = ft_itoa(nb);
+		return (write(1, nb_str, ft_strlen(nb_str)));
+	}
 	else if (nxt == 'u')
-
+	{
+		c = va_arg(arg, char);
+		return (write(1, &c, 1));
+	}
 	else if (nxt == 'x')
-
+	{
+		c = va_arg(arg, char);
+		return (write(1, &c, 1));
+	}
 	else if (nxt == 'X')
+	{
+		c = va_arg(arg, char);
+		return (write(1, &c, 1));
+	}
 }
 
-int	ft_printf(const char*, ...)
+int	ft_printf(const char *format, ...)
 {
     int i;
     int size;
@@ -41,7 +80,7 @@ int	ft_printf(const char*, ...)
        if (format[i] == '%')
        {
 			i++;
-            ft_conversion(arg, nxt); //a modifier
+            size += ft_conversion(arg, format[i]);
        }
 	   else
 	   {
